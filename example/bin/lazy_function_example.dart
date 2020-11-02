7b3ae42a812567cb50a7186a635703e703a8fc5c
simphotonics <git@simphotonics.com>
2020-11-02 18:13:46 +0000

    Initial upload.

---
+++ example/bin/lazy_function_example.dart
@@ -0,0 +1,67 @@
+import 'package:lazy_memo/lazy_memo.dart';
+
+// Computationally expensive function:
+int _factorial(int x) => (x == 0 || x == 1) ? 1 : x * _factorial(x - 1);
+
+/// Returns the value of the polynomial:
+/// `c.first + c[1]*x + ... + c.last* pow(x, c.length)`,
+/// where the entries of `c` represent the polynomial coefficients.
+num _polynomial(num x, Iterable<num> c) {
+  if (c.isEmpty) {
+    return 0;
+  } else if (c.length == 1) {
+    return c.first;
+  } else {
+    return c.first + x * _polynomial(x, c.skip(1));
+  }
+}
+
+// To run this program navigate to
+// the folder 'lazy/example'
+// in your terminal and type:
+//
+// # dart --enable-experiment==non-nullable bin/lazy_function_example.dart
+//
+// followed by enter.
+void main() {
+  print('Running lazy_function_example.dart.\n');
+
+  // Lazy function
+
+  // Lazy function
+  final factorial = LazyFunction<int, int>((x) => _factorial(x));
+
+  print('-------- Factorial ------------');
+  print('Calculates and stores the result');
+  print('factorial(12) = ${factorial(12)}\n');
+
+  // The current function table
+  print('Function table:');
+  print(factorial.functionTable);
+  print('');
+
+  // Returning a cached result.
+  print('Cached result:');
+  print('factorial(12) = ${factorial(12)}');
+
+  // Lazy function with two arguments
+  final polynomial = LazyFunction2(_polynomial);
+  print('\n-------- Polynomial ------------');
+  print('Calculates and stores the result of: ');
+
+  print('polynomial(2, [2, -9, 10, 11, 15]): ${polynomial(2, [
+    2,
+    -9,
+    10,
+    11,
+    15
+  ])}');
+  print('');
+
+  print('The current function table');
+  print(polynomial.functionTable);
+  print('');
+
+  print('Returns a cached result.');
+  print(polynomial(2, [2, -9, 10, 11, 15]));
+}

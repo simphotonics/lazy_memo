526349906d3cfe44aee5453658125d111e017808
simphotonics <git@simphotonics.com>
2021-03-11 13:34:50 +0000

    Amended docs.

--- lib/lazy_memo.dart
+++ lib/lazy_memo.dart
@@ -1,6 +1,5 @@
-/// Provides the generic class `Lazy<T>`.
-///
-/// Enables the creation of cached objects with late initialization.
+/// Provides helper classes for the creation of *lazy variables*
+/// and *memoized functions*.
 library lazy_memo;
 
 export 'src/lazy.dart';

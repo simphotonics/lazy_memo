20eac3a2da4344a7575415df0c494883299b47d6
simphotonics <git@simphotonics.com>
2023-07-02 06:21:47 +0100

    Added benchmarks.

---
+++ benchmark/bin/memoized_function_benchmark.dart
@@ -0,0 +1,67 @@
+import 'package:benchmark_harness/benchmark_harness.dart';
+import 'package:lazy_memo/lazy_memo.dart';
+
+late BigInt a;
+final BigInt arg = BigInt.from(50);
+
+// Create a new benchmark by extending BenchmarkBase
+class FactorialMemoBenchmark extends BenchmarkBase {
+  const FactorialMemoBenchmark() : super('factorial(50)                    ');
+
+  static void main() {
+    const FactorialMemoBenchmark().report();
+  }
+
+  // The benchmark code.
+  @override
+  void run() {
+    a = factorial(arg);
+  }
+
+  // Not measured setup code executed prior to the benchmark runs.
+  @override
+  void setup() {}
+
+  // Not measured teardown code executed after the benchmark runs.
+  @override
+  void teardown() {}
+
+  // To opt into the reporting the time per run() instead of per 10 run() calls.
+  //@override
+  //void exercise() => run();
+}
+
+// Create a new benchmark by extending BenchmarkBase
+class FactorialBenchmark extends BenchmarkBase {
+  const FactorialBenchmark() : super('factorial.(50, recalculate: true)');
+
+  static void main() {
+    const FactorialBenchmark().report();
+  }
+
+  // The benchmark code.
+  @override
+  void run() {
+    a = factorial(
+      arg,
+      recalculate: true,
+    );
+  }
+
+  // Not measured setup code executed prior to the benchmark runs.
+  @override
+  void setup() {}
+
+  // Not measured teardown code executed after the benchmark runs.
+  @override
+  void teardown() {}
+
+  // To opt into the reporting the time per run() instead of per 10 run() calls.
+  //@override
+  //void exercise() => run();
+}
+
+void main() {
+  FactorialMemoBenchmark.main();
+  FactorialBenchmark.main();
+}

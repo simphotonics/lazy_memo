1199617dde2a8f7a159d4c56ab8b54197ff555aa
simphotonics <git@simphotonics.com>
2026-08-26 19:45:11 +0100

    Added GenericMemoizedFunction. Renamed MemoizedFunction -> MemoizedSingleArgumentFunction.

--- lib/src/memoized_functions.dart
+++ lib/src/memoized_functions.dart
@@ -1,46 +1,105 @@
-/// A function requiring a single argument of type `A` and
-/// returning an object of type `T`.
-typedef SingleArgumentFunction<A, T> = T Function(A arg);
+/// A generic function with type argument [T] and
+/// and return type [R].
+typedef GenericFunction<R> = R Function<T>();
+
+/// A function requiring a single argument of type [A] and
+/// returning an object of type [R].
+typedef SingleArgumentFunction<R, A> = R Function(A arg);
 
 /// A function requiring arguments of type `A1, A2` and
-/// returning an object of type `T`.
-typedef DoubleArgumentFunction<A1, A2, T> = T Function(A1 arg1, A2 arg2);
+/// returning an object of type [R].
+typedef DoubleArgumentFunction<R, A1, A2> = R Function(A1 arg1, A2 arg2);
 
-/// Class representing a memoized function
-/// requiring a single argument of type `A`
-/// and returning an object of type `T`.
-class MemoizedFunction<A, T> {
-  /// Constructs a instance of [MemoizedFunction].
-  /// * `func`: A function with signature: `A Function(T)`.
-  /// * `functionTable`: may be used to
+/// Class representing a generic memoized function
+/// with *one* type parameter [T], no arguments and return type [R].
+class GenericMemoizedFunction<R> {
+  /// Constructs a instance of [GenericMemoizedFunction].
+  /// * [func]: A function with signature: `R Function<T>()`.
+  /// * [functionTable]: May be used to
   ///   initialize the function lookup table with
-  ///   {function argument: function value pairs}.
-  MemoizedFunction(this.func, {Map<A, T> functionTable = const {}}) {
+  ///   {type: function value pairs}.
+  new(this.func, {Map<Type, R> functionTable = const {}}) {
     if (functionTable.isNotEmpty) {
       _functionTable.addAll(functionTable);
     }
   }
 
-  /// Returns the function argument type `A`.
-  Type get argumentType => A;
-
-  /// Returns the function return type `T`.
-  Type get returnType => T;
+  /// Returns the function return type [R].
+  Type get returnType => R;
 
   /// Returns the typedef of the memoized function.
-  Type get signature => SingleArgumentFunction<A, T>;
+  Type get signature => GenericFunction<R>;
 
   /// Function being memoized.
-  final SingleArgumentFunction<A, T> func;
+  final GenericFunction<R> func;
 
   /// Function table.
-  final _functionTable = <A, T>{};
+  final _functionTable = <Type, R>{};
+
+  /// Returns the result of calling `func<T>()` or a cached result if available.
+  /// * The cache is initialized when first accessed.
+  /// * To re-initialize the cached function result use the
+  ///   optional parameter `recalculate`.
+  R call<T>({bool recalculate = false}) {
+    if (recalculate) {
+      return _functionTable[T] = func<T>();
+    } else {
+      return _functionTable[T] ??= func<T>();
+    }
+  }
+
+  /// Clears the cached function table.
+  ///
+  /// To clear the function table for selected values only,
+  /// provide a non-empty iterable `args`.
+  void clearFunctionTable({Iterable<Type>? args}) {
+    if (args == null) {
+      _functionTable.clear();
+    } else {
+      for (var key in args) {
+        _functionTable.remove(key);
+      }
+    }
+  }
+
+  /// Returns a copy of the current function table.
+  Map<Type, R> get functionTable => Map<Type, R>.of(_functionTable);
+}
+
+/// Class representing a memoized function
+/// with return type [R] and argument type [A].
+class MemoizedSingleArgumentFunction<R, A> {
+  /// Constructs a instance of [MemoizedSingleArgumentFunction].
+  /// * [func]: A function with signature: `R Function(A)`.
+  /// * [functionTable]: May be used to
+  ///   initialize the function lookup table with
+  ///   {function argument: function value pairs}.
+  new(this.func, {Map<A, R> functionTable = const {}}) {
+    if (functionTable.isNotEmpty) {
+      _functionTable.addAll(functionTable);
+    }
+  }
+
+  /// Returns the function argument type [A].
+  Type get argumentType => A;
+
+  /// Returns the function return type [R].
+  Type get returnType => R;
+
+  /// Returns the typedef of the memoized function.
+  Type get signature => SingleArgumentFunction<R, A>;
+
+  /// Function being memoized.
+  final SingleArgumentFunction<R, A> func;
+
+  /// Function table.
+  final _functionTable = <A, R>{};
 
   /// Returns the result of calling `func` or a cached result if available.
   /// * The cache is initialized when first accessed.
   /// * To re-initialize the cached function result use the
   ///   optional parameter `recalculate`.
-  T call(A arg, {bool recalculate = false}) {
+  R call(A arg, {bool recalculate = false}) {
     if (recalculate) {
       return _functionTable[arg] = func(arg);
     } else {
@@ -63,43 +122,43 @@ class MemoizedFunction<A, T> {
   }
 
   /// Returns a copy of the current function table.
-  Map<A, T> get functionTable => Map<A, T>.from(_functionTable);
+  Map<A, R> get functionTable => Map<A, R>.of(_functionTable);
 }
 
 /// Class representing a memoized function requiring arguments of type
-/// `A1` and `A2`, respectively, and returning an object of type `T`.
-class MemoizedFunction2<A1, A2, T> {
+/// [A1] and [A2], respectively, and returning an object of type [R].
+class MemoizedDoubleArgumentFunction<R, A1, A2> {
   /// Constructor:
   /// The parameter `functionTable` may be used to
   /// initialize the function lookup table with certain
   /// {function argument1: {function argument 2: function value pairs} }
   /// entries.
-  MemoizedFunction2(this.func, {Map<A1, Map<A2, T>> functionTable = const {}}) {
+  new(this.func, {Map<A1, Map<A2, R>> functionTable = const {}}) {
     if (functionTable.isNotEmpty) {
       _functionTable.addAll(functionTable);
     }
   }
 
   /// The memoized function.
-  final DoubleArgumentFunction<A1, A2, T> func;
+  final DoubleArgumentFunction<R, A1, A2> func;
 
   /// Function table
-  final _functionTable = <A1, Map<A2, T>>{};
+  final _functionTable = <A1, Map<A2, R>>{};
 
   /// Returns the function argument types `[A1, A2]`.
   List<Type> get argumentTypes => [A1, A2];
 
-  /// Returns the function return type `T`.
-  Type get returnType => T;
+  /// Returns the function return type [R].
+  Type get returnType => R;
 
   /// Returns the typedef of the memoized function.
-  Type get signature => DoubleArgumentFunction<A1, A2, T>;
+  Type get signature => DoubleArgumentFunction<A1, A2, R>;
 
   /// Returns the result of calling `func` or a cached result if available.
   /// * The cache is initialized when first accessed.
   /// * To re-initialize the cached function result use the
   ///   optional parameter `recalculate`.
-  T call(A1 arg1, A2 arg2, {bool recalculate = false}) {
+  R call(A1 arg1, A2 arg2, {bool recalculate = false}) {
     if (recalculate) {
       _functionTable[arg1] = {arg2: func(arg1, arg2)};
       return _functionTable[arg1]![arg2]!;
@@ -114,8 +173,8 @@ class MemoizedFunction2<A1, A2, T> {
   }
 
   /// Returns a copy of the current function table.
-  Map<A1, Map<A2, T>> get functionTable =>
-      Map<A1, Map<A2, T>>.of(_functionTable);
+  Map<A1, Map<A2, R>> get functionTable =>
+      Map<A1, Map<A2, R>>.of(_functionTable);
 
   /// Clears the cached function table.
   void clearFunctionTable() {

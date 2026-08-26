dc0a0b1342987c3c9e5c0d71620322868d697503
simphotonics <git@simphotonics.com>
2026-08-26 19:42:03 +0100

    Updated examples.

--- example/bin/lazy_list_example.dart
+++ example/bin/lazy_list_example.dart
@@ -1,20 +1,23 @@
 import 'package:lazy_memo/lazy_memo.dart';
 
 void main(List<String> args) {
-  final origin = <String>['zero', 'one', 'two'];
+  final original = <String>['zero', 'one', 'two'];
 
-  var lazyList = LazyList<String>(() => [...origin, 'three']);
+  final lazyList = LazyList<String>(() => original);
 
   // The object
   print(lazyList);
-  // The cached list view
-  print(lazyList());
 
   // Returns the same object till it is re-initialized.
-  print(lazyList() == lazyList());
+  print('\nlazyList() == lazyList():');
+  print('  ${lazyList() == lazyList()}');
+
+  print('\nAdding an element to the original list.');
+  print('original.add(\'three\');');
+  original.add('three');
+
+  print('\nlazyList() == lazyList(updateCache: true):');
   print(lazyList() == lazyList(updateCache: true));
 
-  origin.add('four');
-
-  print(lazyList(updateCache: true));
+  print('\n$lazyList');
 }
--- example/bin/memoized_function_example.dart
+++ example/bin/memoized_function_example.dart
@@ -1,49 +1,29 @@
 import 'package:lazy_memo/lazy_memo.dart';
 
 /// Computationally expensive function with a single argument.
-int _factorial(int x) => (x == 0 || x == 1) ? 1 : x * _factorial(x - 1);
-
-/// Returns the factorial of a positive integer.
-final factorial = MemoizedFunction(
-  _factorial,
-  functionTable: {8: 40320}, // Optional initial function table.
-);
-
-/// Computationally expensive function with two arguments.
-int _combinations(int n, int k) {
-  if (k > n ~/ 2) {
-    return _combinations(n, n - k);
-  } else if (k > n) {
-    return 0;
+BigInt _factorial(BigInt x) {
+  if (x == BigInt.zero || x == BigInt.one) {
+    return BigInt.one;
+  } else if (x > BigInt.zero) {
+    return x * _factorial(x - BigInt.one);
   } else {
-    int result = 1;
-    int m = 1;
-    for (var i = n; i > n - k; i--) {
-      result = (result * i) ~/ m;
-      m++;
-    }
-    return result;
+    throw ArgumentError.value(x, 'x', 'Not defined for negative values!');
   }
 }
 
-/// Returns the number of k-combinations of n distinct objects. More formally,
-/// let S be a set containing n distinct objects.
-/// Then the number of subsets containing k objects is given by combinations(n, k).
-/// * combinations(n, n) = 1
-/// * combinations(n, k) = combinations(n, n - k)
-/// * combinations(n, 0) = 1
-final combinations = MemoizedFunction2(_combinations);
+/// Returns the factorial of a positive integer. Throws and error of type
+/// [ArgumentError] if a negative argument is provided.
+final factorial = MemoizedSingleArgumentFunction(
+  _factorial,
+  functionTable: {12.big: 479001600.big},
+);
 
-// To run this program navigate to
-// the root folder of your local copy of 'lazy_memo' and use the command:
-//
-// # dart example/bin/lazy_function_example.dart
 void main() {
-  print('Running lazy_function_example.dart.\n');
+  print('Running memoized_function_example.dart.\n');
 
   print('------------- Factorial --------------');
   print('Calculates and stores the result');
-  print('factorial(12) = ${factorial(49)}\n');
+  print('factorial(49) = ${factorial(49.big)}\n');
 
   // The current function table
   print('Function table:');
@@ -52,18 +32,5 @@ void main() {
 
   // Returning a cached result.
   print('Cached result:');
-  print('factorial(12) = ${factorial(12)}');
-
-  print('\n----- k-combinations of n objects -----');
-
-  print('Calculates and stores the result of: ');
-  print('combinations(10, 5): ${combinations(100, 70)}');
-  print('');
-
-  print('The current function table');
-  print(combinations.functionTable);
-  print('');
-
-  print('Returns a cached result.');
-  print('combinations(10, 5): ${combinations(10, 5)}');
+  print('factorial(12) = ${factorial(12.big)}');
 }

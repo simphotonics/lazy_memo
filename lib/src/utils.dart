9422836ee519c13101d8b7f7b52f6289ad3527e4
simphotonics <git@simphotonics.com>
2026-08-27 16:27:10 +0100

    Updated docs.

--- lib/src/utils.dart
+++ lib/src/utils.dart
@@ -1,46 +1,47 @@
 import 'memoized_function.dart';
 
+/// Returns the factorial of [x]. Throws an [ArgumentError] if x < 0.
 BigInt _factorial(BigInt x) {
   if (x == BigInt.zero || x == BigInt.one) {
     return BigInt.one;
   } else if (x > BigInt.zero) {
-    return x * _factorial(x - BigInt.one);
+    return x * factorial(x - BigInt.one);
   } else {
-    throw ArgumentError.value(x, 'x', 'Not defined for negative values!');
+    throw ArgumentError('Not defined for negative values: Found: x = $x.');
   }
 }
 
-/// Returns the factorial of a positive integer. Throws and error of type
-/// [ArgumentError] if a negative argument is provided.
+/// ```Dart
+/// factorial(BigInt x)
+/// ```
+/// * Returns the factorial of a positive integer `x` of type [BigInt].
+/// * Throws an error of type
+///   [ArgumentError] if a negative argument is provided.
+/// * To easily convert an [int] to [BigInt] use the getter [ToBigInt.big].
 final factorial = MemoizedSingleArgumentFunction(_factorial);
 
 extension ToBigInt on int {
-  /// Converts this to [BigInt].
+  /// Converts the integer to [BigInt].
   BigInt get big => BigInt.from(this);
 }
 
 /// Returns the number of k-combination of n distinct objects.
 BigInt _combinations(BigInt n, BigInt k) {
   if (n < BigInt.zero) {
-    throw ArgumentError.value(n, 'n', 'Arguments must be positive.');
+    throw ArgumentError('Arguments must be positive. Found n = $n.');
   }
 
   if (k < BigInt.zero) {
-    throw ArgumentError.value(k, 'k', 'Argument must be positive.');
+    throw ArgumentError('Argument must be positive. Found k = $k.');
   }
 
   if (n < k) {
-    throw ArgumentError.value(
-      k,
-      'k',
-      'combinations($n, k) requires k <= $n. '
-          'Found k',
-    );
+    throw ArgumentError('combinations($n, k) requires k <= $n. Found k = $k.');
   }
 
   if (k > n ~/ BigInt.two) {
-    return _combinations(n, n - k);
-  } else {
+    return combinations(n, n - k);
+  } else if (true) {
     var result = BigInt.one;
     var m = BigInt.one;
     for (var i = n; i > n - k; i = i - BigInt.one) {
@@ -51,13 +52,17 @@ BigInt _combinations(BigInt n, BigInt k) {
   }
 }
 
-/// Returns the number of k-combinations of n distinct objects. More formally,
+/// ```Dart
+/// combinations(BigInt n, BigInt k)
+/// ```
+/// Returns the number of k-combinations of n distinct objects as a [BigInt].
+///
+/// More formally,
 /// let S be a set containing n distinct objects.
-/// Then the number of subsets containing k objects is given by
-/// combinations(n, k).
+/// Then the number of subsets containing k objects is given by:
 /// * combinations(n, n) = 1
 /// * combinations(n, k) = combinations(n, n - k)
 /// * combinations(n, 0) = 1
-/// Throws an error of type [ArgumentError] if a negative argument is provided
-/// or if n < k.
+/// * Throws an error of type [ArgumentError] if a negative argument is provided
+///   or if n < k.
 final combinations = MemoizedDoubleArgumentFunction(_combinations);
